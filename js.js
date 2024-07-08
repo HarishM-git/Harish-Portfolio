3165831a26f67c5ff615bbc7011e1cdf34dea1ee
harishm-git <harishdeepakh@gmail.com>
2024-07-08 22:54:31 +0530

    added the js for header color at corresponding window

--- js.js
+++ js.js
@@ -270,6 +270,31 @@ Projects.forEach((element)=>{
 
 
 
+// header.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.middle-section a');
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').includes(current)) {
+        link.classList.add('active');
+      }
+    });
+  });
+});
 
 
 
