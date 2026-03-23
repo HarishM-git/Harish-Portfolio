4995490d827f3fff529b019789c02e3087f6b36d
HarishM-git <harishdeepakh@gmail.com>
2026-03-23 15:44:03 +0530

    totally changed to my new portfolio

--- for-loop.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as gui
-import time
-m=input("enter a message:")
-n=input('total number:')
-time.sleep(2)
-for i in range(int(n)):
-  gui.typewrite(m)
-  gui.press('Enter')
-  
--- sum.py
+++
@@ -1,5 +0,0 @@
-with open("file.txt", "w") as f:
-    f.write("Created using write mode.")
-
-f = open("file.txt","r")
-print(f.read())
